9a6922e224fc361d1d93187cd5b7ec3fde744179
Ernesto <ejcontrerast@outlook.com>
2024-11-24 21:56:43 -0800

    Feat: Continue adding style and fixing issues

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -5,8 +5,10 @@ import Board from "./Board";
 
 const Game: React.FC = () => {
   return (
-    <div className="flex items-center justify-center w-dvw h-dvh bg-[#c2c2c2] border-4 border-t-white border-b-[#999] border-l-white border-r-[#999]">
-      <h1 className="font-extrabold font-blue ">Board Game</h1>
+    <div className=" w-auto h-max-100
+    flex flex-col items-center justify-center
+    bg-[#c2c2c2] border-4 border-t-white border-b-[#999] border-l-white border-r-[#999]">
+      <h1 className="font-extrabold text-blue-700 text-5xl ">Board Game</h1>
       <Header />
       <Board />
     </div>
--- src/components/NumberDisplay.tsx
+++ src/components/NumberDisplay.tsx
@@ -7,8 +7,10 @@ interface NumberDisplayProps {
 const NumberDisplay: React.FC<NumberDisplayProps> = ({value}) => {
   return (
     <div className="flex justify-center items-center">
-      <div className="w-20 h-12
-      text-center text-[#ff0701] bg-black">{value.toString().padStart(3,"0")}</div>
+      <div className="w-20 h-12 text-4xl
+      text-center text-[#ff0701] bg-black m-1">
+        {value.toString().padStart(3,"0")}
+      </div>
     </div>
   )
 }
