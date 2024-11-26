3dd7a7f503db1a77f3228a002710135a10a7bd3b
Ernesto <ejcontrerast@outlook.com>
2024-11-26 15:44:40 -0800

    Feat: added style to the buttons/cells when it is pressed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,9 +11,9 @@ const Button: React.FC = () => {
       className="
       w-7 h-7
       border-4 border-l-white border-r-[#7b7b7b] border-t-white border-b-[#7b7b7b]
-      focus:outline-none"
+      focus:outline-none
+      active:border-t-[#7b7b7b] active:border-b-white active:border-l-[#7b7b7b] active:border-r-white"
     >
-      
     </button>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,13 @@ import NumberDisplay from './NumberDisplay';
 
 const Header: React.FC = () => {
   return (
-    <main className= "bg-[#c0c0c0] py-2 px-3 border-4 border-r-white border-b-white border-l-[#7b7b7b] border-t-[#7b7b7b]    flex items-center space-between"
+    <div className= 
+    "bg-[#c0c0c0] py-2 px-3 border-4 border-r-white border-b-white border-l-[#7b7b7b] border-t-[#7b7b7b] flex items-center justify-between"
     >
       <NumberDisplay value={0} />
       <Face />
       <NumberDisplay value={23} />
-    </main>
+    </div>
   );
 };
 
